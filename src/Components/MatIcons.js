de2196c8976b7938f9ea09a525c2b2e807d22858
mlessin <roymukunde@gmail.com>
2020-01-27 12:06:23 +0300

    designed icons

--- src/Components/MatIcons.js
+++ src/Components/MatIcons.js
@@ -1,20 +1,34 @@
 import React from 'react';
-
+import '../App.css';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import InstagramIcon from '@material-ui/icons/Instagram';
-import FeedbackIcon from '@material-ui/icons/Feedback';
+//import FeedbackIcon from '@material-ui/icons/Feedback';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import EmailIcon from '@material-ui/icons/Email';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles(theme => ({
+    horizontalSpacing: {
+      marginLeft: '5%',
+    }
+  }));
+  
 function MatIcons() {
+    const classes = useStyles();
     return (
         <div className='Icons'>
+            <a><span className={classes.horizontalSpacing}>FAQS</span></a>
 
-            <TwitterIcon />
-            <InstagramIcon />
-            <FeedbackIcon />
-            <FacebookIcon />
-            <EmailIcon />
+            <a><span className={classes.horizontalSpacing}>Feedback</span></a>
+
+            <TwitterIcon className={classes.horizontalSpacing}/>
+            <InstagramIcon className={classes.horizontalSpacing}/>
+            
+            <FacebookIcon className={classes.horizontalSpacing}/>
+            <EmailIcon className={classes.horizontalSpacing}/>
+
+
         </div>
     )
 
