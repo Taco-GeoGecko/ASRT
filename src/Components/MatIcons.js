dea788f4e93aa820d5ad7ad1dbef0550666d477d
mlessin <roymukunde@gmail.com>
2020-01-30 14:56:54 +0300

    styled maticons

--- src/Components/MatIcons.js
+++ src/Components/MatIcons.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import '../App.css';
+import Instagram from '../Images/Instagram.png'
+import { makeStyles } from '@material-ui/core/styles';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import InstagramIcon from '@material-ui/icons/Instagram';
+import FaceBook from '../Images/FaceBook.png';
+import Gmail from '../Images/Gmail.svg';
 //import FeedbackIcon from '@material-ui/icons/Feedback';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import EmailIcon from '@material-ui/icons/Email';
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import { blue, red } from '@material-ui/core/colors';
 
 
 const useStyles = makeStyles(theme => ({
   horizontalSpacing: {
+    marginLeft: 5,
+  },
+  navigation: {
+    height: 20,
+    width: 25,
     marginLeft: '5%',
-  }
+    },
 }));
 
 function MatIcons() {
   const classes = useStyles();
   return (
     <div className='Icons'>
-      <a><span className={classes.horizontalSpacing}>FAQS</span></a>
+      <a href='https://www.geogecko.com/'><span className={classes.horizontalSpacing}>FAQS</span></a>
 
-      <a><span className={classes.horizontalSpacing}>Feedback</span></a>
+      <a href='https://www.geogecko.com/'><span className={classes.horizontalSpacing}>Feedback</span></a>
 
       <a href='https://twitter.com/geogecko'><TwitterIcon className={classes.horizontalSpacing} style={{ color: blue[500] }} /></a>
 
-      <a><InstagramIcon className={classes.horizontalSpacing} /></a>
+      <a href='https://www.instagram.com/geogecko1/'><img src={Instagram} target="blank" alt="geo gecko logo" className={classes.navigation} /></a>
 
-      <a href="https://www.facebook.com/GeoGeckoUganda/"><FacebookIcon className={classes.horizontalSpacing} style={{ color: blue[500] }} /></a>
-      <EmailIcon className={classes.horizontalSpacing} />
+      <a href='https://www.facebook.com/geogecko/'><img src={FaceBook} target="blank" alt="geo gecko logo" className={classes.navigation} /></a>
+
+      <a href='https://www.facebook.com/geogecko/'><img src={Gmail} target="blank" alt="geo gecko logo" className={classes.navigation} /></a>
 
 
     </div>
