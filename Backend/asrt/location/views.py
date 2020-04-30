b313dbe96d20e80fd5130729b69c19405f7d90c1
lambert3 <lygobalambert@outlook.com>
2020-04-30 14:57:20 +0300

    reduced the padding of dropdown of filters

--- Backend/asrt/location/views.py
+++ Backend/asrt/location/views.py
@@ -11,7 +11,7 @@ def view(request):
         # establishing a connection
     connection = psycopg2.connect(
                                     user = "postgres",
-                                    password = "508282044",
+                                    password = "lambtex",
                                     host = "localhost",
 
                                     port = "5432",
