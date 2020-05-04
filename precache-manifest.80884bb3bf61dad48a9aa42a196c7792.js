979cf542b96b4a6be4d757050bf7b9be43b4e80c
lambert3 <lygobalambert@outlook.com>
2020-05-04 15:42:04 +0300

    Updates

---
+++ precache-manifest.80884bb3bf61dad48a9aa42a196c7792.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86bb3fe56733c6f9d0f606043daa4b9c",
+    "url": "/ASRT/index.html"
+  },
+  {
+    "revision": "00ab311586e0bf7dc274",
+    "url": "/ASRT/static/css/2.ba87ed55.chunk.css"
+  },
+  {
+    "revision": "aff7807779fa5f74b698",
+    "url": "/ASRT/static/css/main.f59aa5a7.chunk.css"
+  },
+  {
+    "revision": "00ab311586e0bf7dc274",
+    "url": "/ASRT/static/js/2.8392d5ad.chunk.js"
+  },
+  {
+    "revision": "bff0dea366aca0eb341932f9f5879dad",
+    "url": "/ASRT/static/js/2.8392d5ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aff7807779fa5f74b698",
+    "url": "/ASRT/static/js/main.7ba0a543.chunk.js"
+  },
+  {
+    "revision": "937ebd0094c15548f9ee",
+    "url": "/ASRT/static/js/runtime-main.b716013d.js"
+  },
+  {
+    "revision": "e67c6af8237338b75bc64b81a1c7d096",
+    "url": "/ASRT/static/media/Gmail.e67c6af8.svg"
+  },
+  {
+    "revision": "3d8e0c91f729082ea990024182de9f6b",
+    "url": "/ASRT/static/media/Instagram.3d8e0c91.png"
+  },
+  {
+    "revision": "ccb98897d3d994ab9b3604827026b070",
+    "url": "/ASRT/static/media/geoLogo.ccb98897.png"
+  },
+  {
+    "revision": "efa2921636f27785032aec07502000ba",
+    "url": "/ASRT/static/media/geogecko.efa29216.png"
+  }
+]);
